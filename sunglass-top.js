df983763e28ce5ed69aecae999999171aaae444d
komitti <ryousukekomiya0207@gmail.com>
2020-08-29 00:06:24 +0900

    create sunglass-top.js

---
+++ sunglass-top.js
@@ -0,0 +1,12 @@
+var icon = document.getElementById('icon');
+icon.innerHTML =
+'<li>'+
+'<a href="https://twitter.com/ksu_scs">'+
+'<img src="Twitter_logo.png" class="twitter" width="50px" height="50px">'+
+'</a>'+
+'</li>'+
+'<li>'+
+'<a href="https://www.instagram.com/sunglass.ksu/">'+
+'<img src="Insta_logo.png" class="instagram" width="50px" height="50px">'+
+'</a>'+
+'</li>';
