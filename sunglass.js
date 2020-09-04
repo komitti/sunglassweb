9d7dded2e5690fe1420e9c44c4b9257c053cbe6a
komitti <ryousukekomiya0207@gmail.com>
2020-09-04 23:44:59 +0900

    exchange for picture/?

--- sunglass.js
+++ sunglass.js
@@ -2,28 +2,28 @@ var icon = document.getElementById('icon');
 icon.innerHTML =
 '<li>'+
 '<a href="https://twitter.com/ksu_scs">'+
-'<img src="Twitter_logo.png" class="twitter" width="50px" height="50px">'+
+'<img src="picture/Twitter_logo.png" class="twitter" width="50px" height="50px">'+
 '</a>'+
 '</li>'+
 '<li>'+
 '<a href="https://www.instagram.com/sunglass.ksu/">'+
-'<img src="Insta_logo.png" class="instagram" width="50px" height="50px">'+
+'<img src="picture/Insta_logo.png" class="instagram" width="50px" height="50px">'+
 '</a>'+
 '</li>';
 
 var title = document.getElementById('title');
 title.innerHTML =
 '<h2 id="sub-title">サイエンスコミュニケーション研究会</h2>'+
-'<img src="sunglass_logo.png" id="logo" alt="sunglass" width="87px" height="87px">'+
+'<img src="picture/sunglass_logo.png" id="logo" alt="sunglass" width="87px" height="87px">'+
 '<h1>サングラス</h1>'+
-'<h2 id="title-english">sunglass</h2>';
+'<h2 id="title-english">sunglasses</h2>';
 
 var menu = document.getElementById('menu');
 menu.innerHTML =
 '<ul>'+
 '<li><a href="sunglass-home.html">ホーム<div>home</div></a></li>'+
 '<li><a href="sunglass-experiment.html">おもしろい科学実験<div>experiment</div></a></li>'+
-'<li><a href="sunglass-kagakunomori.html">科学の森<div>forest</div></a></li>'+
+'<li><a href="/sunglass-kagakunomori.html">科学の森<div>forest</div></a></li>'+
 '<li><a href="">各種活動記録<div>activity</div></a></li>'+
 '<li><a href="sunglass-member.html">メンバー紹介<div>member</div></a></li>'+
 '</ul>';
