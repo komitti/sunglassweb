f192b20c8e8fd458f85505b34a510322b2c638b8
komitti <ryousukekomiya0207@gmail.com>
2020-09-06 14:54:48 +0900

    change something

--- experiment/sunglass.js
+++ experiment/sunglass.js
@@ -2,19 +2,28 @@ var icon = document.getElementById('icon');
 icon.innerHTML =
 '<li>'+
 '<a href="https://twitter.com/ksu_scs">'+
-'<img src="picture/Twitter_logo.webp" class="twitter" width="50px" height="50px">'+
+'<picture>'+
+'<source srcset="picture/Twitter_logo.webp" type="image/webp">'+
+'<img src="picture/Twitter_logo.jpg" alt="Twitter" width="50px" height="50px">'+
+'</picture>'+
 '</a>'+
 '</li>'+
 '<li>'+
 '<a href="https://www.instagram.com/sunglass.ksu/">'+
-'<img src="picture/Insta_logo.webp" class="instagram" width="50px" height="50px">'+
+'<picture>'+
+'<source srcset="picture/Insta_logo.webp" type="image/webp">'+
+'<img src="picture/Insta_logo.jpg" alt="Instagram" width="50px" height="50px">'+
+'</picture>'+
 '</a>'+
 '</li>';
 
 var title = document.getElementById('title');
 title.innerHTML =
 '<h2 id="sub-title">サイエンスコミュニケーション研究会</h2>'+
-'<img src="picture/sunglass_logo.webp" id="logo" alt="sunglass" width="87px" height="87px">'+
+'<picture>'+
+'<source srcset="picture/sunglass_logo.webp" type="image/webp">'+
+'<img src="picture/sunglass_logo.jpg" alt="Sunglasses" id="logo" width="87px" height="87px">'+
+'</picture>'+
 '<h1>サングラス</h1>'+
 '<h2 id="title-english">sunglasses</h2>';
 
@@ -22,10 +31,11 @@ var menu = document.getElementById('menu');
 menu.innerHTML =
 '<ul>'+
 '<li><a href="../sunglass-home.html">ホーム<div>home</div></a></li>'+
-'<li><a href="../sunglass-experiment.html">おもしろい科学実験<div>experiment</div></a></li>'+
+'<li><a href="../sunglass-experiment.html">面白い科学実験<div>experiment</div></a></li>'+
 '<li><a href="../sunglass-kagakunomori.html">科学の森<div>forest</div></a></li>'+
 '<li><a href="../sunglass-katsudou.html">各種活動記録<div>activity</div></a></li>'+
 '<li><a href="../sunglass-member.html">メンバー紹介<div>member</div></a></li>'+
+'<li><a href="../sunglass-question.html">質問コーナー<div>question</div></a></li>'+
 '</ul>';
 
 var copyright = document.getElementById('copyright');
