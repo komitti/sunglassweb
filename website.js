c5536866de9173cbb13f3b682cc505b55f2be0c6
komitti <ryousukekomiya0207@gmail.com>
2021-02-12 20:46:04 +0900

    create website.js

---
+++ website.js
@@ -0,0 +1,19 @@
+const website = [
+  {datetime: '2021-02-12', time: '2021/02/12', span: 'Ver.1.0.0', p: 'サングラスのホームページを投稿しました'},
+];
+
+let web_li = document.querySelector('#website');
+for (var i = 0; i < website.length; i++) {
+  let web = website[i];
+  let clone = web_li.content.cloneNode(true);
+  const time = clone.querySelector('time');
+  const span = clone.querySelector('span');
+  const p = clone.querySelector('p');
+
+  time.setAttribute('datetime', web.datetime);
+  time.innerHTML = web.time;
+  span.innerHTML = web.span;
+  p.innerHTML = web.p;
+
+  web_li.parentNode.appendChild(clone);
+};
