d52d373a2ce9f4607a0205bc4f6488b873272a3d
komitti <ryousukekomiya0207@gmail.com>
2021-02-20 02:24:05 +0900

    add science forest URL.

--- navigation.js
+++ navigation.js
@@ -2,7 +2,7 @@ const navigation = [
   {href: 'sunglass-home.html', src: 'picture/home_icon.png', cha: 'ホーム'},
   {href: 'sunglass-summary.html', src: 'picture/sunglass_logo.png', cha: '概要'},
   //{href: 'sunglass-experiment.html', src: 'picture/experiment_icon.png', cha: '面白い科学実験'},
-  //{href: 'sunglass-forest.html', src: 'picture/forest_icon.png', cha: '科学の森'},
+  {href: 'sunglass-scienceforest.html', src: 'picture/forest_icon.png', cha: '科学の森'},
   //{href: 'sunglass-activity.html', src: 'picture/activity_icon.png', cha: '各種活動記録'},
   //{href: 'sunglass-member.html', src: 'picture/member_icon.png', cha: 'メンバー紹介'},
   //{href: 'sunglass-q&a.html', src: 'picture/q&a_icon.png', cha: '質問コーナー'},
